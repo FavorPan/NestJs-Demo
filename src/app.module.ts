6f3687a5a1c17134360a94b5665d6f69c649fe7f
Favor.P <favor.pan@sjfood.com>
2022-12-14 17:14:47 -0800

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './server/user/user.module';
+
+const DBModule = MongooseModule.forRoot('mongodb://localhost/demoDB');
+
+@Module({
+  imports: [DBModule, UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
